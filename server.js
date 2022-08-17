110a79ff2069e0ce29abfd8e78d32347876bdc62
gabrielcoder247 <gabrielcoder247@gmail.com>
2022-08-17 14:57:34 +0100

    Edit package.json

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const app = express();
 app.use(express.static(__dirname + '/dist/<app-name>'));
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname +
-        '/dist/<app-name>/index.html'));
+        '/dist/smart-tayfun/index.html'));
 });
 app.listen(process.env.PORT || 8080);
